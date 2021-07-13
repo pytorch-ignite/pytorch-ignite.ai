ae8ad42fb08c710734e4dc285e97ad7f98b6f43a
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-07-13 14:13:12 +0630

    add copy code button

---
+++ assets/js/copy-code.js
@@ -0,0 +1,31 @@
+// reference from
+// https://www.dannyguo.com/blog/how-to-add-copy-to-clipboard-buttons-to-code-blocks-in-hugo/
+function copyCode() {
+  document.querySelectorAll("pre > code[data-lang]").forEach(function (value) {
+    const button = document.createElement("button");
+    button.className = "copy-code-button";
+    button.type = "button";
+    button.innerText = "Copy";
+
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      navigator.clipboard
+        .writeText(value.innerText)
+        .then(function () {
+          button.blur();
+          button.innerText = "Copied";
+          alert("Code has been copied to clipboard.");
+          setTimeout(function () {
+            button.innerText = "Copy";
+          }, 1500);
+        })
+        .catch(function (reason) {
+          console.error(`${reason} Error copying code.`);
+        });
+    });
+
+    value.closest(".highlight").insertAdjacentElement("afterbegin", button);
+  });
+}
+copyCode();
