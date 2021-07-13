92712fd5a95655ad040d57c32e082c264a145457
Taras Savchyn <30748114+trsvchn@users.noreply.github.com>
2021-07-13 19:23:38 +0300

    Update nb_to_md.sh

--- scripts/nb_to_md.sh
+++ scripts/nb_to_md.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-# Usage: # nb_to_md.sh <notebook_dir> <target_dir>
+# Usage: nb_to_md.sh <notebook_dir> <target_dir>
 # Example: ./scripts/nb_to_md.sh examples ./content/docs/tutorials
-
 # Convert from ipynb to plain md
 jupyter nbconvert --to markdown $1/*.ipynb --output-dir $2 --NbConvertApp.output_files_dir=../../../static/images/notebooks
 # Make Hugo compatible md
