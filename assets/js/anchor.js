440fa45a488250b26a66be74aea3ef7d479e540a
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-09-05 22:05:37 +0630

    fix: try with content

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -2,7 +2,6 @@ document.querySelectorAll('h2, h3').forEach((value) => {
   const child = document.createElement('a')
   child.href = '#' + value.id
   child.className = 'header-anchor'
-  child.innerText = '#'
   child.setAttribute('aria-hidden', true)
   value.prepend(child)
 })
