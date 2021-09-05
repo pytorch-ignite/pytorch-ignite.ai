8bd49e513d117e3797589881e48e86b968efa67f
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-09-05 22:13:08 +0630

    fix: add span tag

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -2,6 +2,7 @@ document.querySelectorAll('h2, h3').forEach((value) => {
   const child = document.createElement('a')
   child.href = '#' + value.id
   child.className = 'header-anchor'
+  child.innerHTML = `<span class="sr-only">#${value.id}</span>`
   child.setAttribute('aria-hidden', true)
   value.prepend(child)
 })
