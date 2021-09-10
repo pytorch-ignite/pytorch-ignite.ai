1130885250d8440cc74d9c07c0f1f4513f233416
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-09-10 15:34:33 +0630

    wip: style for main content area (#69)

--- assets/js/anchor.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('h2, h3').forEach((value) => {
-  const child = document.createElement('a')
-  child.href = '#' + value.id
-  child.className = 'header-anchor'
-  child.innerText = '#'
-  child.setAttribute('aria-hidden', true)
-  value.prepend(child)
-})
