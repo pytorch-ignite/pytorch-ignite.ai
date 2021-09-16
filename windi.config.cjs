59316ba56cb97387e63fd92c0311d2d43a07acd7
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-09-16 10:44:14 +0630

    underline

--- windi.config.cjs
+++ windi.config.cjs
@@ -1,7 +1,6 @@
-const { defineConfig } = require("windicss/helpers")
+const { defineConfig } = require('windicss/helpers')
 const typography = require('windicss/plugin/typography')
 const colors = require('windicss/colors')
-
 
 module.exports = defineConfig({
   darkMode: 'class',
@@ -11,17 +10,20 @@ module.exports = defineConfig({
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Inter var, Inter, ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"'
+        sans: 'Inter var, Inter, ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
       },
       typography: {
         DEFAULT: {
           css: {
             maxWidth: 'none',
-            a: { textDecoration: 'none' },
+            a: {
+              textDecoration: 'none',
+              '&:hover': { textDecoration: 'underline' },
+            },
             code: { color: colors.violet[500] },
-          }
-        }
-      }
-    }
-  }
+          },
+        },
+      },
+    },
+  },
 })
